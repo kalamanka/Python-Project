ce23b352f1cc9d726f74c67eb1f9d2877b67016d
Kalamanka <42943232@qq.com>
2022-01-16 22:02:20 +0800

    调试可以使用的读取写入excel文件模块

--- 内部审计项目/readexcelfile.py
+++ 内部审计项目/readexcelfile.py
@@ -18,10 +18,14 @@ import xlrd
 #print s.row(r)
 
 #excel文件类
-class ExcelFile(object):    
-    ExcelFileBook = NULL
+class ExcelFile:    
+    #Excel工作簿
+    Book = NULL
+    CurrentSheet = NULL
+    CurrentCell = NULL
+
     #打开文件函数
-    def open_file(strPath):
+    def __open_file(self,strPath):
         ret = False
         ret = os.path.exists(strPath)
         if(ret):
@@ -29,19 +33,41 @@ class ExcelFile(object):
             print(ret)    
         return ret,strPath
     #打开Excel文件，赋值给excelFileBook
-    def open_excel_file(strFileName):
-        ret = open_file(strFileName)
-        if(ret[0]):
+    def open_excel_file(self,strFileName):
+        global Book
+        retValue = self.__open_file(strFileName)
+        if(retValue[0]):
             #文件存在
-            strFileName = ret[1]
+            strFileName = retValue[1]
             #打开excel文件
-            ExcelFileBook = xlrd.open_workbook(strFileName)
+            self.Book = xlrd.open_workbook(strFileName)
 
+    #打开Excel文件中的Sheet，需要给出Sheet名称
+    def open_sheet(self,sheetName):
+        # 获取第1个表单
+        self.CurrentSheet = self.Book.sheet_by_name(sheetName)
+
+    #打开Excel文件中的Sheet中的单元格，需要Sheet名称，行列序号。
+    def open_cell(self,sheetName,col,row):
+        self.open_sheet(sheetName)
+        self.CurrentCell = self.CurrentSheet.cell_value(col,row)
 
 #打开excel文件
 excelFile  = ExcelFile()
 excelFile.open_excel_file("d:\学员信息导入.xls")
+excelFile.open_sheet("Sheet1")
+excelFile.open_cell("Sheet1",0,0)
 
+print ("表单数量:", excelFile.Book.nsheets)
+print ("表单名称:", excelFile.Book.sheet_names())
+print (u"表单 %s 共 %d 行 %d 列" % (excelFile.CurrentSheet.name, excelFile.CurrentSheet.nrows,excelFile.CurrentSheet.ncols))
+print ("第0行第0列:", excelFile.CurrentCell)
+
+for sheetItem in excelFile.Book.sheets():
+    for r in range(sheetItem.nrows):
+        print ("行内容：" ,sheetItem.row(r))
+
+# 输出指定行
 #df = pd.read_excel("")
 #读取excel文件内容
 #data=df.head(2)
